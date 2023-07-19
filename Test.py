b4dde7278f84e6627afffcf247674af9d17ae617
lastchance45 <122393769+lastchance45@users.noreply.github.com>
2023-07-19 21:52:31 +0000

    testestestes

--- Test.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-years = range(2000,2012)
-yield_apples = [1,2,3,4,5,6,7,8,9,10]
-plt.plot(years, yield_apples)
---
+++ test_graph.py
@@ -0,0 +1,7 @@
+""" This is to try to use graphs to create items"""
+import matplotlib.pyplot as plt
+
+print('Hello World')
+years = range(2000,2012)
+yield_apples = [1,2,3,4,5,6,7,8,9,10]
+plt.plot(years, yield_apples)
